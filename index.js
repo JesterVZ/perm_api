cde85583a002c0d7a187b50dec21b40ec0bdcaa6
Jester <justjestervz@gmail.com>
2024-04-09 19:31:39 +0500

    new logic

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
 const mysql = require('mysql2');
 const express = require("express");
 const app = express();
-var cors = require('cors')
+const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 
-const connection = mysql.createConnection({
-  port: '3303',
-  host: '178.253.40.144',
-  user: 'root',
-  password: '112233',
-  database: 'perm_dp'
-});
+class Database {
+  constructor() {
+    this.connection = mysql.createConnection({
+      port: '3303',
+      host: '178.253.40.144',
+      user: 'root',
+      password: '112233',
+      database: 'perm_dp'
+    });
+    this.makeConnection();
+  }
 
-function makeConnection(){
-  connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting to database: ' + err.stack);
-      return;
+  makeConnection() {
+    this.connection.connect((err) => {
+      if (err) {
+        console.error('Error connecting to database: ' + err.stack);
+        return;
+      }
+      console.log('Connected to database as id ' + this.connection.threadId);
+    });
+  }
+
+  getQuestionsAndAnswers(req, res) {
+    try {
+      this.connection.query('SELECT * FROM questions', (err, questionResults) => {
+        if (err) {
+          console.error('Error fetching questions: ' + err.stack);
+          res.status(500).send('Error fetching questions');
+          return;
+        }
+        const questions = questionResults;
+        
+        this.connection.query('SELECT * FROM answers', (err, answerResults) => {
+          if (err) {
+            res.status(500).send('Error fetching answers');
+            return;
+          }
+          const answers = answerResults;
+          res.json({
+            'questions': questions,
+            'answers': answers
+          });
+        });
+      });
+    } catch (e) {
+      this.makeConnection();
     }
-    console.log('Connected to database as id ' + connection.threadId);
-  });
+  }
 }
-
-makeConnection();
-
 
 
 
 app.get('/question', (req, res) => {
-  try{
-    connection.query('SELECT * FROM questions', (err, results) => {
-      if (err) {
-        console.error('Error fetching question: ' + err.stack);
-        res.status(500).send('Error fetching question');
-        return;
-      }
-      const question = results;
-      connection.query('SELECT * FROM answers', (err, results) => {
-        if (err) {
-          res.status(500).send('Error fetching answers');
-          return;
-        }
-        const answers = results;
-        res.json({
-          'questions': question,
-          'answers': answers
-        });
-      });
-      
-    });
-  }catch(e){
-    makeConnection();
-  }
-    
-  });
+  const db = new Database();
+  db.getQuestionsAndAnswers(req, res);
+});
 
-  app.listen(3000);
+app.listen(3000);
